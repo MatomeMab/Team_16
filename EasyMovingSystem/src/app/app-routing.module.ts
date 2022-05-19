cd7b3ea238dc118d317630d3112aa4321234c43f
MatomeMab <62009843+MatomeMab@users.noreply.github.com>
2022-05-20 00:16:16 +0200

    Update "New Nav"

--- EasyMovingSystem/src/app/app-routing.module.ts
+++ EasyMovingSystem/src/app/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TruckComponent } from './Admin/Components/Truck/truck/truck.component';
 import { EmployeeComponent } from './Admin/Components/Employee/employee/employee.component';
+import { EmployeeTypeComponent } from './Admin/Components/EmployeeType/employee-type/employee-type.component';
 
 const routes: Routes = [ 
   {
     path: 'Employee', 
     component: EmployeeComponent
   },
-  {path: '', component: TruckComponent}
-
+  { 
+    path: 'Truck', 
+    component: TruckComponent
+  },
+  {
+    path: 'EmployeeType', 
+    component: EmployeeTypeComponent
+  }
 ];
 
 @NgModule({
